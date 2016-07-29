4e90437c953299cfcef7d111445144f797d2ec75
AvanKempen <info@avkempen.nl>
2016-07-29 14:06:50 +0200

    Changed date

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Vishandel Cronj&eacute;</title>
+		
+		<!-- META -->
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="pragma" content="no-cache" />
+		<meta name="author" content="AvanKempen" />
+		<meta name="web_author" content="AvanKempen" />
+		<meta name="copyright" content="Copyright AvanKempen 2016" />
+		
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+		
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="static/css/style.css" />
+		<link rel="stylesheet" type="text/css" href="static/css/icons.css" />
+		
+		<!-- jQuery -->
+		<script type="text/javascript" src="static/js/jquery-3.0.0.min.js"></script>
+		<script type="text/javascript" src="static/js/core.js"></script>
+		<script type="text/javascript" src="static/js/modules/home.js"></script>
+	</head>
+	
+	<body data-module="home">
+		<header>
+			<div class="container add-space">
+				<div class="logo"></div>
+				<h1>Vishandel Cronj&eacute;</h1>
+			</div>
+		</header>
+		
+		<div class="container block-white">
+			<section>
+				<div class="add-space">
+					<div class="column-3">
+						<img src="static/images/winkel.jpg" alt="Winkel" />
+					</div>
+					
+					<div class="column-9">
+						<h2 class="text-blue">Welkom bij Vishandel Cronj&eacute;</h2>
+						
+						<p class="lead">
+							De Cronjéstraat heeft nu ook eindelijk een viswinkel, en dat werd tijd ook!
+							De winkel is een aanwinst voor de straat want een viswinkel was wat nog ontbrak in de Cronjéstraat.
+						</p>
+						
+						<p>
+							 In de winkel zelf is een groot en divers assortiment te vinden van heerlijke visproducten. Van zelf schoongemaakte haringen tot dagverse vis. En natuurlijk is ook de gebakken vis aanwezig; onder meer heerlijke warmgebakken kibbeling en lekkerbek (beiden van echte kabeljauw) zijn onze specialiteiten. Maar ook zelfgemaakte salades en zelf samen te stellen visschotels zijn bij ons verkrijgbaar.
+						</p>
+						
+						<p>
+							Dus kom en laat u verrassen door (h)eerlijke producten van hoge kwaliteit bij Vishandel Cronj&eacute;!
+						</p>
+					</div>
+					
+					<div class="clear"></div>
+					
+					<hr />
+					
+					<div class="column-12">
+						<h3 class="text-blue">Een greep van ons menu</h3>
+					</div>
+					
+					<div class="clear"></div>
+				</div>
+			</section>
+			
+			<footer>
+				<div class="add-space">
+					<div class="column-4">
+						<h4>Contact</h4>
+						<p>
+							<strong>T:</strong> <a href="tel:0649588924">06 49 58 89 24</a><br />
+							<strong>E:</strong> info@vishandelcronje.nl
+						</p>
+					</div>
+					
+					<div class="column-4">
+						<h4>Bezoek ons</h4>
+						<p>
+							Generaal Cronj&eacute;straat 36<br />2021 JJ Haarlem
+						</p>
+					</div>
+					
+					<div class="column-4">
+						<h4>Openingstijden</h4>
+						<p>
+							Dagelijks geopend van 09:00 tot 20:00
+						</p>
+					</div>
+					
+					<div class="clear"></div>
+					
+					<div class="column-12">
+						<i>
+							<p>
+							Copyright &copy; Vishandel Cronj&eacute; <?=date('Y');?> - Concept &amp; realisatie <a href="http://www.avkempen.nl" class="underline">Av Kempen</a>
+							</p>
+						</i>
+					</div>
+					
+					<div class="clear"></div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
